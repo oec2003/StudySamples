f194208efeab1ee646bbc355e66287f023396462
oec2003 <oec2003@gmail.com>
2019-10-10 23:36:16 +0800

    add rabbitmqdemo

---
+++ RabbitMQDLXDemo/Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1", UserName = "oec2003", Password = "123456" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                    channel.QueueDeclare("queue-2", false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body); 
+                        Console.WriteLine("已接收： {0}", message);   
+                    };
+                    channel.BasicConsume("queue-2", false, consumer);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ RabbitMQDLXDemo/Producter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1", UserName = "oec2003", Password = "123456" };
+            using (var connection = factory.CreateConnection())
+            while (Console.ReadLine() != null)
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var arguments = new Dictionary<string, object>();
+                    arguments.Add("x-dead-letter-exchange", "exchange-2");
+                    arguments.Add("x-dead-letter-routing-key", "rk-2");
+
+                    channel.QueueDeclare("queue-1",true,false,false,arguments);
+                
+                    channel.ExchangeDeclare("exchange-2", "direct");
+                    channel.QueueDeclare("queue-2", false, false, false, null);
+                    channel.QueueBind("queue-2", "exchange-2", "rk-2", null);
+
+                    var message = "Hello oec2003!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    properties.Expiration = "5000";
+
+                    channel.BasicPublish("", "queue-1", properties, body);
+                    Console.WriteLine($"发送： {message}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
